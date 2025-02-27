f5db7cc31dbb6e5beb8143ae9af3f5943e019ac8
samuel fasanya <samuelfasanya351@gmail.com>
2025-02-27 11:05:10 +0100

    readme live site

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -3,7 +3,8 @@ import { logout } from "../services/authService.ts";
 
 const apiClient = axios.create({
   // baseURL: "http://localhost:5000/api/v1",
-  baseURL: "http://ec2-13-247-101-35.af-south-1.compute.amazonaws.com/api/v1",
+  // baseURL: "http://ec2-13-247-101-35.af-south-1.compute.amazonaws.com/api/v1",
+  baseURL: "https://51f8-13-247-101-35.ngrok-free.app/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
