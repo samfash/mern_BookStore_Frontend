535ecfae4568b9c9b2fb5b0553cf1f13414e1569
samuel fasanya <samuelfasanya351@gmail.com>
2025-03-05 14:47:40 +0100

    placegolder fix

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -3,7 +3,6 @@ import { logout } from "../services/authService.ts";
 
 const apiClient = axios.create({
   // baseURL: "http://localhost:5000/api/v1",
-  // baseURL: "http://ec2-13-247-101-35.af-south-1.compute.amazonaws.com/api/v1",
   baseURL: " https://definite-unduly-cicada.ngrok-free.app/api/v1",
   headers: {
     "Content-Type": "application/json",
